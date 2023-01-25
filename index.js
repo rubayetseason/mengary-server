a69a06d0f548caa19649786f9afa0be0eaf5dd06
rubayet-islam <season.ss2001@gmail.com>
2023-01-25 22:19:11 +0600

    get endpoint created for all products

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 require("dotenv").config();
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@cluster0.tsmlaiu.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
-
-app.get('/' , (req, res) => {
-    res.send('Mengary server running');
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const productsCollection = client.db("mengary").collection("products");
+
+    app.get("/products", async (req, res) => {
+      let query = {};
+      const products = await productsCollection.find(query).toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Mengary server running");
+});
 
 app.listen(port, () => {
-    console.log('mengary server running');
-})
+  console.log("mengary server running");
+});
