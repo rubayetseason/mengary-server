49d2d4c3d1ad5104fa43d17e4ef8a5970e52d5c6
rubayet-islam <season.ss2001@gmail.com>
2023-01-25 22:10:37 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 require("dotenv").config();
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@cluster0.tsmlaiu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 app.get('/' , (req, res) => {
